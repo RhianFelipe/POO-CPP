4bf6459988a24ae576de8290f1e70bb6bd8f98ed
Rhian Felipe Silva de Lara <62002116+RhianFelipe@users.noreply.github.com>
2021-03-20 22:25:18 -0300

    Formatando Data estilo DD/MM/AA com <iomanip>

--- C++/Exercicios_C++/FormatandoDate.cpp
+++ C++/Exercicios_C++/FormatandoDate.cpp
@@ -1,7 +1,7 @@
 
 #include <iostream>
 #include <iomanip>//  Biblioteza que fornece uma hora em uma estrutura de horas e uma cadeia de caracteres de formato para ser usada
-                 //Permite o ciclo conveniente de cadeias de caracteres, com operadores de inserção e extração
+                 //Permite o ciclo conveniente de cadeias de caracteres, com operadores de inserÃ§Ã£o e extraÃ§Ã£o
 using namespace std;
 int main()
 {
@@ -13,8 +13,7 @@ cout << "Mes:";
 cin >> M ;
 cout << "Ano:";
 cin >> A;
-//setw para usar a condição se tiver menos de 2 casas, e o setfill para colcoar o 0 caso a condição seja concebida.
+//setw para usar a condiÃ§Ã£o se tiver menos de 2 casas, e o setfill para colcoar o 0 caso a condiÃ§Ã£o seja concebida.
 cout << endl << "A data formatada: " << setw(2) << setfill('0')<<  D  << "/" << setw(2) << M << "/" << A;
-
 }
 
