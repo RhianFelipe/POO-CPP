59e4585b4a00388ffc09f27b3d10ea2ff1945dfa
Rhian Felipe Silva de Lara <62002116+RhianFelipe@users.noreply.github.com>
2021-04-09 22:11:24 -0300

    Data Update
    Arquivos separados por .h e main

---
+++ ExerciciosLogicosCpp/POO_C++/data/main.cpp
@@ -0,0 +1,16 @@
+#include "data.h"
+
+
+int main() {
+Data d1(5,4,2021), d2(1,1,2020);
+
+d1.mostra();
+d1.mostraAnoBissexto();
+d1.mostraTrimestre();
+d1.mostraNumDiasMes();
+
+d2.mostra();
+d2.mostraAnoBissexto();
+d2.mostraTrimestre();
+d2.mostraNumDiasMes();
+}
