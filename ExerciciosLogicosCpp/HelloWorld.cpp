e60d3800b432ef0e4086c2f8453bd2e5d9aa1d23
Rhian Felipe Silva de Lara <62002116+RhianFelipe@users.noreply.github.com>
2021-05-01 16:48:41 -0300

    Update HelloWorld.cpp

--- ExerciciosLogicosCpp/HelloWorld.cpp
+++ ExerciciosLogicosCpp/HelloWorld.cpp
@@ -1,6 +1,7 @@
 #include <iostream> // manipulação de fluxo de dados padrão do sistema (entrada padrão, saída padrão e saída de erros padrão)
 #include <string> //Para acessar funções de string, como a própria variavel string
 using namespace std;  // o certo é que todas as funções tenham std:: antes dela, mas com esse comando não precisamos acrescentar
+
 int main(void){
 	//para printar qualquer digitação utilizamos a função "cout"
 	
@@ -10,7 +11,7 @@ int main(void){
 	//printando variaveis:
 	int a = 5; //variavel do tipo inteiro que recebe o valor 5
 	cout << a << "\n"; //para cada nova escrita, devemos utilizar o "<<" 
-	
+
 	// outro exemplo de cout
 	cout << "O valor da variavel 'a' =" << a << "\n"; // o \n para descer até a proxima linha/Quebra linha
 }
