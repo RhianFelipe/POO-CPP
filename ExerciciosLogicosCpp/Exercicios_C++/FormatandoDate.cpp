6d635a8ad009ae14d10b5c3811410e40fa5caf24
Rhian Felipe Silva de Lara <62002116+RhianFelipe@users.noreply.github.com>
2021-06-02 18:06:12 -0300

    Update FormatandoDate.cpp

--- ExerciciosLogicosCpp/Exercicios_C++/FormatandoDate.cpp
+++ ExerciciosLogicosCpp/Exercicios_C++/FormatandoDate.cpp
@@ -1,4 +1,3 @@
-
 #include <iostream>
 #include <iomanip>//  Biblioteza que fornece uma hora em uma estrutura de horas e uma cadeia de caracteres de formato para ser usada
                  //Permite o ciclo conveniente de cadeias de caracteres, com operadores de inserção e extração
@@ -16,4 +15,3 @@ cin >> A;
 //setw para usar a condição se tiver menos de 2 casas, e o setfill para colcoar o 0 caso a condição seja concebida.
 cout << endl << "A data formatada: " << setw(2) << setfill('0')<<  D  << "/" << setw(2) << M << "/" << A;
 }
-
