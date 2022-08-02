29ad65b7375c536e38205ac08cff0c03f9e6800f
Rhian Felipe Silva de Lara <62002116+RhianFelipe@users.noreply.github.com>
2022-08-02 14:38:25 -0300

    Update data.h

--- ExerciciosLogicosCpp/POO/Aula4-Sobrecarga/data/data.h
+++ ExerciciosLogicosCpp/POO/Aula4-Sobrecarga/data/data.h
@@ -7,7 +7,38 @@ int dia;
 int mes;
 int ano;
 
+public:#include <cstdio>
+#include <iostream>
+
+
+class Data {
+int _dia;
+int _mes;
+int _ano;
+
 public:
+
+Data(int dia , int mes , int ano ):_dia(dia),_mes(mes),_ano(ano) {}
+
+~Data(){}
+void mostra();
+
+int getDia()const{return _dia;}
+int getMes()const{return _mes;}
+int getAno()const{return _ano;}
+
+//void setDia(int dia){if(dia <= 1 and dia >= numDiaMes())this->dia = dia; else printf("Dia invalido!");}
+
+bool anoBissexto();
+void mostraAnoBissexto();
+int trimestre();
+void mostraTrimestre();
+int numDiasMes();
+void mostraNumDiasMes();
+
+};
+
+
 
 Data(int , int , int );
 ~Data();
